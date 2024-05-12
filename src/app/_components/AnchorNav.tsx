269700259fde22f751682ac253c89c8b8622cb33
hyeonjun-L <beeminor60@gmail.com>
2024-05-12 23:39:07 +0900

    feat: Anchor Nav view point 표현 구현

--- src/app/_components/AnchorNav.tsx
+++ src/app/_components/AnchorNav.tsx
@@ -2,15 +2,21 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { HIDE_PATH } from '@/constants/constants';
 import { Headings } from '@/types/types';
 
 const AnchorNav = () => {
   const [headings, setHeadings] = useState<Headings[]>([]);
+  const [viewHeadings, setViewHeadings] = useState<Headings[]>([]);
   const pathname = usePathname();
-  const marginRef = useRef(0);
-  const initialMargin = 5;
+
+  const viewHeading = viewHeadings.reduce<Headings>(
+    (acc, heading) => {
+      return acc.level < heading.level ? acc : heading;
+    },
+    { level: 7, text: '' },
+  );
 
   useEffect(() => {
     const allHeadings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
@@ -24,40 +30,78 @@ const AnchorNav = () => {
         };
       },
     );
-    marginRef.current = 0;
     setHeadings(newHeadings);
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const target = entry.target as HTMLElement;
+          const headingText = target?.innerText;
+          const headingLevel = Number(target.nodeName.slice(1));
+
+          if (entry.isIntersecting) {
+            setViewHeadings((prevHeadings) => {
+              const exists = prevHeadings.some(
+                (heading) => heading.text === headingText,
+              );
+              if (!exists) {
+                return [
+                  ...prevHeadings,
+                  { level: headingLevel, text: headingText },
+                ];
+              }
+              return prevHeadings;
+            });
+          } else {
+            setViewHeadings((prevHeadings) =>
+              prevHeadings.filter((heading) => heading.text !== headingText),
+            );
+          }
+        });
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1,
+      },
+    );
+
+    allHeadings.forEach((heading) => {
+      observer.observe(heading);
+    });
+
+    return () => {
+      allHeadings.forEach((heading) => {
+        observer.unobserve(heading);
+      });
+    };
   }, [pathname]);
 
-  const calculateMargin = (beforeLevel: number, currentLevel: number) => {
-    if (beforeLevel < currentLevel) {
-      marginRef.current += initialMargin;
-    } else if (beforeLevel > currentLevel) {
-      marginRef.current -= initialMargin * (beforeLevel - currentLevel);
-    }
-  };
+  const ANCHOR_HEADER_MARGIN = ['', 'pl-4', 'pl-8', 'pl-12', 'pl-16', 'pl-20'];
 
   return (
     !HIDE_PATH.includes(pathname) && (
-      <aside className="bg-White-anchor-bg hidden h-full w-72 flex-shrink-0 overflow-y-auto px-6 py-5 text-sm lg:block dark:bg-dark-anchor-bg">
-        <section className="text-dark-600 flex max-h-full w-full flex-col gap-1 border-l border-solid border-black dark:border-white">
-          {headings.map(({ level, text }, index) => {
-            const beforeLevel = index > 0 ? headings[index - 1].level : level;
-
-            calculateMargin(beforeLevel, level);
-
-            const mlStyle = { paddingLeft: `${marginRef.current}px` };
+      <aside className="pl- hidden h-full w-72 flex-shrink-0 overflow-y-auto bg-White-anchor-bg p-5 text-sm lg:block dark:bg-dark-anchor-bg">
+        <section className="text-dark-600 flex max-h-full w-full flex-col gap-1 border-l border-solid border-black pl-1 dark:border-white">
+          {headings.map(({ level: currentLevel, text }, index) => {
+            const beforeLevel = headings[index - 1]?.level;
+            const hasHigherPrevLevel =
+              beforeLevel && beforeLevel !== currentLevel;
 
             return (
               <div
-                style={mlStyle}
                 key={index}
-                className="has-[:hover]:bg-White-anchor-hover flex w-full dark:has-[:hover]:bg-dark-anchor-hover"
+                className={`flex w-full has-[:hover]:bg-White-anchor-hover dark:has-[:hover]:bg-dark-anchor-hover 
+                ${viewHeading.text === text ? 'bg-dark-anchor-active' : ''}`}
               >
                 <Link
                   href={`#${text}`}
-                  className="line-clamp-2 max-w-full break-words dark:hover:text-white"
+                  className={`${ANCHOR_HEADER_MARGIN[--currentLevel]} relative line-clamp-2 max-w-full break-words dark:hover:text-white`}
                   replace
                 >
+                  {hasHigherPrevLevel && (
+                    <span className="absolute -translate-x-5">┗</span>
+                  )}
                   {text}
                 </Link>
               </div>
