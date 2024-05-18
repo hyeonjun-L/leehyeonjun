7bdbfce4d4640d80a8047a64f38917a0bddb0a3e
hyeonjun-L <beeminor60@gmail.com>
2024-05-19 03:28:07 +0900

    feat: anchorNav viewHeading 없는 경우 예외처리 기능 추가

--- src/app/_components/AnchorNav.tsx
+++ src/app/_components/AnchorNav.tsx
@@ -11,10 +11,8 @@ const AnchorNav = () => {
   const [viewHeadings, setViewHeadings] = useState<Headings[]>([]);
   const pathname = usePathname();
 
-  const viewHeading = viewHeadings.reduce<Headings>(
-    (acc, heading) => {
-      return acc.level < heading.level ? acc : heading;
-    },
+  const viewHeading = viewHeadings.reduce(
+    (acc, heading) => (acc.level < heading.level ? acc : heading),
     { level: 7, text: '', id: '' },
   );
 
@@ -22,13 +20,17 @@ const AnchorNav = () => {
     return text.replace(/\s+/g, '-').toLowerCase();
   };
 
+  const resetViewHeadings = () => {
+    setViewHeadings([]);
+  };
+
   useEffect(() => {
     const allHeadings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
 
     const newHeadings: Headings[] = Array.from(allHeadings).map(
-      (heading: Element) => {
+      (heading: Element, index) => {
         const element = heading as HTMLElement;
-        const id = generateIdFromText(element.innerText);
+        const id = generateIdFromText(element.innerText + index);
         element.setAttribute('id', id);
         return {
           level: Number(element.localName.slice(1)),
@@ -37,21 +39,21 @@ const AnchorNav = () => {
         };
       },
     );
+
     setHeadings(newHeadings);
 
     const observer = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
-          const target = entry.target as HTMLElement;
-          const headingText = target?.innerText;
+          const { target, isIntersecting, boundingClientRect, rootBounds } =
+            entry;
+          const headingText = (target as HTMLElement).innerText;
           const headingLevel = Number(target.nodeName.slice(1));
           const headingId = target.id;
 
-          if (entry.isIntersecting) {
+          if (isIntersecting) {
             setViewHeadings((prevHeadings) => {
-              const exists = prevHeadings.some(
-                (heading) => heading.text === headingText,
-              );
+              const exists = prevHeadings.some(({ id }) => id === headingId);
               if (!exists) {
                 return [
                   ...prevHeadings,
@@ -61,9 +63,33 @@ const AnchorNav = () => {
               return prevHeadings;
             });
           } else {
-            setViewHeadings((prevHeadings) =>
-              prevHeadings.filter((heading) => heading.text !== headingText),
-            );
+            if (rootBounds && boundingClientRect.top < rootBounds.top) {
+              setViewHeadings((prevHeadings) => {
+                const prevHeadingList = prevHeadings.filter(
+                  ({ id }) => id !== headingId,
+                );
+
+                return prevHeadingList.length === 0
+                  ? prevHeadings
+                  : prevHeadingList;
+              });
+            } else {
+              setViewHeadings((prevHeadings) => {
+                const newHeadings = prevHeadings.filter(
+                  (heading) => heading.id !== headingId,
+                );
+                if (
+                  newHeadings.length === 0 ||
+                  !(rootBounds && boundingClientRect.top < rootBounds.top)
+                ) {
+                  return newHeadings;
+                } else {
+                  const prevIndex =
+                    prevHeadings.findIndex(({ id }) => id === headingId) - 1;
+                  return prevIndex >= 0 ? [prevHeadings[prevIndex]] : [];
+                }
+              });
+            }
           }
         });
       },
@@ -89,7 +115,7 @@ const AnchorNav = () => {
 
   return (
     !HIDE_PATH.includes(pathname) && (
-      <aside className="pl- hidden h-full w-72 flex-shrink-0 overflow-y-auto bg-White-anchor-bg p-5 text-sm lg:block dark:bg-dark-anchor-bg">
+      <aside className="hidden h-full w-72 flex-shrink-0 overflow-y-auto bg-White-anchor-bg p-5 text-sm lg:block dark:bg-dark-anchor-bg">
         <section className="text-dark-600 flex max-h-full w-full flex-col gap-1 border-l border-solid border-black pl-1 dark:border-white">
           {headings.map(({ level: currentLevel, text, id }, index) => {
             const beforeLevel = headings[index - 1]?.level;
@@ -99,11 +125,12 @@ const AnchorNav = () => {
             return (
               <div
                 key={index}
-                className={`flex w-full has-[:hover]:bg-White-anchor-hover dark:has-[:hover]:bg-dark-anchor-hover 
-                ${viewHeading.id === id ? 'bg-dark-anchor-active' : ''}`}
+                className={`flex w-full has-[:hover]:bg-White-anchor-hover dark:has-[:hover]:bg-dark-anchor-hover
+                ${viewHeading.id === id ? 'bg-White-anchor-active dark:bg-dark-anchor-active' : ''}`}
               >
                 <Link
                   href={`#${id}`}
+                  onClick={resetViewHeadings}
                   className={`${ANCHOR_HEADER_MARGIN[--currentLevel]} relative line-clamp-2 max-w-full break-words dark:hover:text-white`}
                   replace
                 >
