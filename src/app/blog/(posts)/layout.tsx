99d684e2ec6d228405c3f14341053005ccc3925d
hyeonjun-L <beeminor60@gmail.com>
2024-06-09 18:03:18 +0900

    refactor: layout 수정

--- src/app/blog/(posts)/layout.tsx
+++ src/app/blog/(posts)/layout.tsx
@@ -4,6 +4,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className="prose max-w-none dark:prose-invert">{children}</section>
+    <section className="prose max-w-none p-3 dark:prose-invert lg:p-8">
+      {children}
+    </section>
   );
 }
