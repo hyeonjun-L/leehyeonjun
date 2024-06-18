4ec78760ac5e5d8aa548ddbc87e4092a74400cda
hyeonjun-L <beeminor60@gmail.com>
2024-06-18 18:05:27 +0900

    feat: 블로그 홈 구현

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { getPosts } from '@/utils/posts';
 import Categories from './_components/Categories';
 import SearchPost from './_components/SearchPost';
+import Image from 'next/image';
 
 export const metadata: Metadata = {
   title: '이현준 | 블로그',
@@ -42,30 +43,48 @@ const page = async ({
   const posts = query ? getFilteredPosts() : postList;
 
   return (
-    <main className="min-h-dvh w-full gap-3 p-4 lg:flex xl:p-8">
+    <main
+      className={`${posts.length > 1 ? '' : 'min-h-dvh'} w-full gap-3 p-4 lg:flex xl:p-8`}
+    >
       <Categories selectCategorie={categorie} posts={postList} />
-      <section className="mx-auto mt-5 lg:mt-0">
-        <div className="flex items-center justify-between py-3">
-          <h1>이현준 블로그</h1>
-          <SearchPost />
+      <section className="mx-auto mt-5 lg:mt-0 lg:w-[44.5rem] xl:w-[47.5rem]">
+        <div className="mb-8">
+          <div className="flex items-center justify-between py-3">
+            <h1>이현준 블로그</h1>
+            <SearchPost />
+          </div>
+          {query && <p>총 {posts.length}개의 포스트를 찾았습니다.</p>}
         </div>
-        <button className="border border-solid border-white p-3">
-          <ol>
-            {posts.map(({ slug, title, publishDate, categories }) => (
-              <li key={slug}>
-                <Link href={`blog/${slug}`}>
-                  <h2>{title}</h2>
-                  <p>
-                    <strong>Published:</strong>
-                    {new Date(publishDate).toLocaleDateString()}
-                    <strong>Categories:</strong>
-                    {categories.map((cat, i) => `${i ? ', ' : ''}${cat}`)}
+        <ol>
+          {posts.map(
+            ({
+              slug,
+              title,
+              description,
+              publishDate,
+              categories,
+              posterImage,
+            }) => (
+              <li key={slug} className="mb-8 border-b border-solid pb-8">
+                <Link href={`blog/${slug}`} className="flex flex-col gap-2">
+                  <div className="relative aspect-video">
+                    <Image
+                      src={posterImage}
+                      alt={`${title} 이미지`}
+                      layout="fill"
+                    />
+                  </div>
+                  <h2 className="text-3xl">{title}</h2>
+                  <p>{description}</p>
+                  <p className="text-White-menu-text dark:text-dark-text">
+                    {new Date(publishDate).toLocaleDateString()} (
+                    {categories.map((cat, i) => `${i ? ', ' : ''}${cat}`)})
                   </p>
                 </Link>
               </li>
-            ))}
-          </ol>
-        </button>
+            ),
+          )}
+        </ol>
       </section>
     </main>
   );
