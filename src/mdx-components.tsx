ceaafcb8ff9016827f81dc105e8d5799a9b7c130
hyeonjun-L <beeminor60@gmail.com>
2024-06-19 10:38:53 +0900

    feat: ReferenceList 공통 컴포넌트 구현

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,9 +1,13 @@
 import type { MDXComponents } from 'mdx/types';
 import BlogTitle from './app/blog/_components/BlogTitle';
+import Link from 'next/link';
+import ReferenceList from './app/blog/_components/ReferenceList';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    Link,
     BlogTitle,
+    ReferenceList,
     ...components,
   };
 }
