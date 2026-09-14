b8b0f9f4c249a1f96f78dbf7e466ee6428f6d245
hyeonjun-L <beeminor60@gmail.com>
2026-09-14 18:10:16 +0900

    refactor: Nav 스타일 수정 & title 내용 수정

--- src/app/_components/ActivityNav.tsx
+++ src/app/_components/ActivityNav.tsx
@@ -68,7 +68,7 @@ const ActivityNav = () => {
         href={href}
         scroll={scroll}
         aria-label={ariaLabel}
-        className="group relative flex aspect-square max-h-20 w-full max-w-20 p-3 sm:size-16 sm:p-4 md:size-20"
+        className="group relative flex size-12 p-3 sm:size-16 sm:p-4 md:size-20"
         target={!href.startsWith('/') ? '_blank' : undefined}
       >
         {isSelected && (
@@ -101,7 +101,7 @@ const ActivityNav = () => {
 
   return (
     <aside className="bg-dark-activity md:h-full dark:bg-dark-activity">
-      <section className="grid h-full grid-cols-[0.66fr_0.33fr] sm:flex sm:flex-col sm:justify-between">
+      <section className="flex h-full justify-between sm:flex-col">
         <nav className="flex sm:block">
           {activityNavList.map(renderNavItem)}
         </nav>
